a74c8ddc06644b79e2c3cd7e887c67bdccb7a44a
Rakotoarilolona <rakotoarilolonahasiniaina@gmail.com>
2026-01-13 09:46:35 +0300

    init

---
+++ clouds5spring/src/main/java/com/carte/clouds5spring/Clouds5springApplication.java
@@ -0,0 +1,13 @@
+package com.carte.clouds5spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Clouds5springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Clouds5springApplication.class, args);
+	}
+
+}
---
+++ clouds5spring/src/test/java/com/carte/clouds5spring/Clouds5springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carte.clouds5spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Clouds5springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
