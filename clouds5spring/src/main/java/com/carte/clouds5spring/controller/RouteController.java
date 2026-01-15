b8f05dee760f8014bd9f37b79bb4a8cd7f5d2717
Rakotoarilolona <rakotoarilolonahasiniaina@gmail.com>
2026-01-15 21:14:51 +0300

    init route

--- clouds5spring/src/main/java/com/carte/clouds5spring/controller/RouteController.java
+++ clouds5spring/src/main/java/com/carte/clouds5spring/controller/RouteController.java
@@ -1,11 +1,23 @@
 package com.carte.clouds5spring.controller;
 
 import org.springframework.web.bind.annotation.*;
-
+import com.carte.clouds5spring.service.Hservice;
 @RestController
 @RequestMapping("/api/data")
 public class RouteController 
 {
-    
-    
+    @GetMapping("/routeprobleme")
+    public String getRouteProbleme() {
+        return Hservice.getProblemeRoutier();
+    }
+
+    @GetMapping("/routeprobleme/{id}")
+    public String getRouteProblemeById(@PathVariable String id) {
+        return Hservice.getProblemeDetail(id);
+    }
+
+    @GetMapping("/routeprobleme/dashboard")
+    public String getRouteProblemeDashboard() {
+        return Hservice.getProblemeDashboard();
+    }
 }
---
+++ clouds5spring/src/main/java/com/carte/clouds5spring/service/Hservice.java
@@ -0,0 +1,17 @@
+package com.carte.clouds5spring.service;
+
+public class Hservice 
+{
+    public static String getProblemeRoutier()
+    {
+        return "Service: Route Probleme Data";
+    }
+    public static String getProblemeDetail(String id)
+    {
+        return "Service: Route Probleme Data for ID: " + id;
+    }
+    public static String getProblemeDashboard()
+    {
+        return "Service: Route Probleme Dashboard Data";
+    }
+}
