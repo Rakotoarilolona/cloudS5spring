94041cbb10712d047529038af90e897c38aa84cd
Toky-Lionnel <tokyrajaonarivony@gmail.com>
2026-01-20 10:18:30 +0300

    synchronisation user postgres -> firebase

--- clouds5spring/src/main/java/com/carte/clouds5spring/config/FirebaseConfig.java
+++ clouds5spring/src/main/java/com/carte/clouds5spring/config/FirebaseConfig.java
@@ -1,8 +1,11 @@
 package com.carte.clouds5spring.config;
 
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
@@ -29,5 +32,9 @@ public class FirebaseConfig
         } catch (Exception e) {
             throw new RuntimeException("Firebase initialization failed", e);
         }
+    }
+
+    public static Firestore getFirestore() {
+        return FirestoreClient.getFirestore();
     }
 }
