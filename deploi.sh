5080eb2c4c09b34e0757688b67bc2857587d437a
Rakotoarilolona <rakotoarilolonahasiniaina@gmail.com>
2026-01-25 11:40:25 +0300

    normal

--- deploi.sh
+++ deploi.sh
@@ -1,12 +1,5 @@
-# 1. Démarrer uniquement la base de données PostgreSQL
-docker compose up -d db
-
-# 2. Vérifier que la base de données est prête
-docker compose logs db
-
-# Attendre de voir ce message : "database system is ready to accept connections"
-
-# 3. Importer les données OSM (une seule fois, peut être long)
-docker compose up osm-import
-
-# L'import terminera automatiquement quand c'est fini
+docker comose up -d db
+sleep 10
+docker compose run osm_import import
+docker compose up -d osm_server
+docker compose up -d app
