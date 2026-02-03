2cc79b551cd55cb1d0f27db6b9ab8a8e37d68beb
Rakotoarilolona <rakotoarilolonahasiniaina@gmail.com>
2026-02-03 11:05:50 +0300

    fonction calcul delais

--- clouds5spring/src/main/java/com/carte/clouds5spring/repository/HistoriqueStatusRouteRepository.java
+++ clouds5spring/src/main/java/com/carte/clouds5spring/repository/HistoriqueStatusRouteRepository.java
@@ -2,7 +2,9 @@ package com.carte.clouds5spring.repository;
 
 import com.carte.clouds5spring.entity.HistoriqueStatusRoute;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 public interface HistoriqueStatusRouteRepository 
         extends JpaRepository<HistoriqueStatusRoute, Integer> {
+
+    List<HistoriqueStatusRoute> findByStatusId(int statusId);
 }
--- clouds5spring/src/main/java/com/carte/clouds5spring/service/Hservice.java
+++ clouds5spring/src/main/java/com/carte/clouds5spring/service/Hservice.java
@@ -8,22 +8,28 @@ import com.carte.clouds5spring.exception.NotFoundException;
 
 
 import com.carte.clouds5spring.hutil.Hjson;
+
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.time.LocalDateTime;
 @Service
 public class Hservice {
 
     private final RouteProblemeRepository routeProblemeRepository;
     private final RouteStatusRepository routeStatusRepository;
+    private final HistoriqueStatusRouteRepository historiqueStatusRouteRepository;
 
     public Hservice(RouteProblemeRepository routeProblemeRepository, 
-        RouteStatusRepository routeStatusRepository) 
+        RouteStatusRepository routeStatusRepository, HistoriqueStatusRouteRepository historiqueStatusRouteRepository) 
     {
         this.routeProblemeRepository = routeProblemeRepository;
         this.routeStatusRepository = routeStatusRepository;
+        this.historiqueStatusRouteRepository = historiqueStatusRouteRepository;
     }
 
     public String getProblemeRoutier() {
@@ -50,7 +56,7 @@ public class Hservice {
         String data = Hjson.toJson(dto);
         return Hjson.formatJson(data, "success", "Data fetched successfully");
     }
-    public StatHistorique getStatHistoriqueByStatus(int id_status)
+    public StatHistorique getStatHistoriqueByStatus(Integer id_status,Integer id_status_suivant)
     {
         StatHistorique stat =new StatHistorique();
         Optional<RouteStatus> status = routeStatusRepository.findById(id_status);
@@ -60,8 +66,41 @@ public class Hservice {
         RouteStatusDto statusDto = status.get().toDto();
         stat.setStatus(statusDto);
         List<HistoriqueStatusRoute> problemeList = historiqueStatusRouteRepository.findByStatusId(id_status);
-
         stat.setProblemeList(problemeList);
+        if(id_status_suivant!=null)
+        {
+            List<HistoriqueStatusRoute> problemeListNext = historiqueStatusRouteRepository.findByStatusId(id_status_suivant);
+            List <Double> delaisList = new ArrayList<>();
+            
+            for(HistoriqueStatusRoute hsr : problemeList)
+            {
+                for(HistoriqueStatusRoute hsrNext : problemeListNext)
+                {
+                    RouteProbleme rp = hsr.getRouteProbleme();
+                    RouteProbleme rpNext = hsrNext.getRouteProbleme();
+                    if(rp.getId() == rpNext.getId())
+                    {
+                        LocalDateTime timestamp = hsr.getDateHistorique();
+                        LocalDateTime timestampNext = hsrNext.getDateHistorique();
+                        double delais = java.time.Duration.between(timestamp, timestampNext).toMinutes();
+                        delaisList.add(delais);
+                        break;
+                    }
+                }
+            }
+            double sommeDelais = 0;
+            for(Double d : delaisList)
+            {
+                sommeDelais += d;
+            }
+            double delaisMoyen = 0;
+            if(delaisList.size() > 0)
+            {
+                delaisMoyen = sommeDelais / delaisList.size();
+            }
+            stat.setDelaisMoyen(delaisMoyen);
+        }
+        
         return stat;
     }
     public String getProblemeDashboard() {
